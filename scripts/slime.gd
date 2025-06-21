a89c4d143e688a52ece7ab3ae293ca15ef1f4131
Greenbeansoup <successparrots@gmail.com>
2025-06-21 15:11:17 -0500

    Adding more states to slime and sleeping again after idle

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -13,11 +13,13 @@ enum Slime_State {
 	ASLEEP,
 	IDLE,
 	AWAKING,
+	CHASE,
 }
 
 var slime_state: Slime_State
 
 var move_timer: Timer
+var sleep_timer: Timer
 var rng: RandomNumberGenerator
 var current_direction: Vector2
 
@@ -26,14 +28,20 @@ var dead = false
 const SPEED = 40
 const IDLE_SPEED = 10
 const AGGRO_RANGE = 100
+const DE_AGGRO_RANGE = 150
 const IDLE_MOVE_TIMEOUT = 1.0
+const SLEEP_TIMEOUT = 5.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_initialize(slime_type)
 	move_timer = Timer.new()
 	move_timer.connect("timeout", _change_direction)
 	move_timer.one_shot = true
+	sleep_timer = Timer.new()
+	sleep_timer.one_shot = true
+	sleep_timer.connect("timeout", _sleep_timer_timeout)
 	add_child(move_timer)
+	add_child(sleep_timer)
 	rng = RandomNumberGenerator.new()
 	health_controller = StatusController.new()
 	health_controller.max_value = 1
@@ -70,14 +78,20 @@ func _physics_process(delta):
 	
 	var direction: Vector2
 	var move_speed = IDLE_SPEED
-	if target != null and target.global_position.distance_to(global_position) <= AGGRO_RANGE:
+	if slime_state != Slime_State.CHASE and target != null and target.global_position.distance_to(global_position) <= AGGRO_RANGE:
 		if slime_state == Slime_State.ASLEEP:
 			_awaken()
 		elif slime_state == Slime_State.IDLE:
-			direction = target.global_position - global_position
-			move_speed = SPEED
+			slime_state = Slime_State.CHASE
 	elif slime_state == Slime_State.IDLE:
 		direction = current_direction
+	elif slime_state == Slime_State.CHASE:
+		if target != null and target.global_position.distance_to(global_position) <= DE_AGGRO_RANGE:
+			direction = target.global_position - global_position
+			move_speed = SPEED
+		else:
+			slime_state = Slime_State.IDLE
+			sleep_timer.start(SLEEP_TIMEOUT)
 	
 	if direction.x > 0:
 		animated_sprite_2d.flip_h = false
@@ -96,6 +110,8 @@ func _awaken():
 
 func _on_animation_finished():
 	var anim_name: String = animated_sprite_2d.animation
+	if slime_state == Slime_State.ASLEEP:
+		return
 	if anim_name == "awaken_purple":
 		move_timer.start(IDLE_MOVE_TIMEOUT + rng.randf())
 		animated_sprite_2d.play("idle_purple")
@@ -113,3 +129,13 @@ func get_random_direction() -> Vector2:
 func _change_direction():
 	current_direction = get_random_direction()
 	move_timer.start(IDLE_MOVE_TIMEOUT + rng.randf())
+
+func _sleep_timer_timeout():
+	print("sleep timeout")
+	if slime_state == Slime_State.IDLE:
+		slime_state = Slime_State.ASLEEP
+		if slime_type == Slime_Type.PURPLE:
+			
+			animated_sprite_2d.play_backwards("awaken_purple")
+		else:
+			animated_sprite_2d.play_backwards("awaken_green")
